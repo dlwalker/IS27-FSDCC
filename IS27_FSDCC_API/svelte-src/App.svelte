2c3404df17d5aa357ad0ecf534afe515eab620e0
dlwalker <danieltrwalker@gmail.com>
2023-05-02 05:15:08 -0700

    completed basic user story requirements for front end -add new position under an existing one -assign an employee to a position

--- IS27_FSDCC_API/svelte-src/App.svelte
+++ IS27_FSDCC_API/svelte-src/App.svelte
@@ -1,102 +1,219 @@
 <script>
-	import TreeNode from './lib/TreeNode.svelte';
+	import EmployeeList from './lib/EmployeeList.svelte';
+import TreeNode from './lib/TreeNode.svelte';
 
 	const url_root = 'API_ROOT_URL';
 
-	function GetStaffDirectory(){
+	function GetStaffDirectory() {
 		const url = url_root + 'staffdirectory';
 
 		fetch(url)
 			.then(response => response.json())
-			.then(data => root = data)
+			.then((data) => root = data)
 			.catch(error => console.error('Failed to retrieve staff directory.', error));
 	}
+	
+	function GetEmployees() {
+		const url = url_root + 'employees';
 
-
-	function UnassignEmployee(position) {
-		position.employee = null;
-		position.employeeNumber = null;
-
-		PutPosition(position);
+		fetch(url)
+			.then(response => response.json())
+			.then(data => employeeList = data)
+			.catch(error => console.error('Failed to retrieve employee list.', error));
 	}
 
-	function PutEmployee(employee) {
+	async function PutEmployee(employee) {
 		const url = url_root + 'employees/' + employee.employeeNumber;
-		fetch(url, {
+		let response = await fetch(url, {
 			method: 'PUT',
 			headers: {
 				'Accept': 'application/json',
       			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(employee)
-		})
-		.then(() => GetStaffDirectory())
-  		.catch(error => console.error('Unable to update item.', error));
+		});
+		
+		if (!response.ok) {
+			throw new Error(response.text());
+		}
 	}
 
-	function PutPosition(position) {
+	async function PostPosition(position){
+		const url = url_root + 'positions/';
+		let response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+      			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(position)
+		});
+
+		if (!response.ok) {
+			throw new Error(response.text());
+		}
+	}
+
+	async function PutPosition(position) {
 		const url = url_root + 'positions/' + position.positionNumber;
-		fetch(url, {
+		let response = await fetch(url, {
 			method: 'PUT',
 			headers: {
 				'Accept': 'application/json',
       			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(position)
+		});
+
+		if (!response.ok) {
+			throw new Error(response.text());
+		}
+	}
+
+	//Opens the form to insert a new position in the hierarchy
+	function ShowNewPositionModal(supervisorPosition) {
+		selectedSuper = supervisorPosition;
+		showPositionModal = true;
+	}
+
+	function CreateNewPosition(newPosition, supervisorPositionNumber) {
+		PostPosition({
+			positionNumber: newPosition.positionNumber,
+			title: newPosition.title,
+			employeeNumber: null,
+			supervisorPositionNumber: supervisorPositionNumber,
 		})
-		.then(() => GetStaffDirectory())
-  		.catch(error => console.error('Unable to update item.', error));
+		.then(GetStaffDirectory)
+		.then(CloseNewPosition)
+		.catch(error => console.error('Failed to create position.', error));
+	}
+
+	function CloseNewPosition() {
+		showPositionModal = false;
 	}
 	
+	//Opens the form for editing an employee's name
 	function ShowEditEmployeeModal(employee) {
 		selectedEmployee = employee;
-		showModal = true;
+		showEmployeeModal = true;
 	}
 
 	function UpdateEmployee(employee){
-		PutEmployee(employee);
-		CloseModal();
+		PutEmployee(employee)
+			.then(GetStaffDirectory)
+			.then(CloseEditEmployee)
+			.catch(error => console.error('Failed to update employee.', error));
 	}
 
-	function CloseModal() {
-		showModal = false;
+	function CloseEditEmployee() {
+		showEmployeeModal = false;
 	}
 	
-	let showModal = false;
-	let selectedEmployee;
+	//Opens the list to select an employee for assignment
+	function ShowEmployeeList(position) {
+		selectedPosition = position;
+		GetEmployees();
+		showEmployeeList = true;
+	}
 
+	function CloseEmployeeList() {
+		showEmployeeList = false;
+	}
+
+	function AssignEmployee(position, employeeNumber) {
+		position.employeeNumber = employeeNumber;
+
+		PutPosition(position)
+			.then(GetStaffDirectory)
+			.then(CloseEmployeeList)
+			.catch(error => console.error('Failed to update position.', error));
+	}
+	
+	function UnassignEmployee(position) {
+		position.employee = null;
+		position.employeeNumber = null;
+
+		PutPosition(position)
+			.then(GetStaffDirectory)
+			.catch(error => console.error('Failed to update position.', error));
+	}
+	
 	let root = null;
+
+	let showPositionModal = false;
+	let selectedSuper = {};
+	$: newPositionTitle = selectedSuper && `Add new position under Position No. ${selectedSuper.positionNumber}:  ${selectedSuper.title}`;
+	let newPosition = {};
+	
+	let showEmployeeModal = false;
+	let selectedEmployee;
+	
+	let showEmployeeList = false;
+	let selectedPosition = {};
+	$: listTitle = selectedPosition && `Assign employee to Position No. ${selectedPosition.positionNumber}:  ${selectedPosition.title}`;
+	let employeeList = [];
+
 	GetStaffDirectory();
 
 </script>
 
 {#if root}
-	<TreeNode node={root} childProp="subordinates" let:node>
-		<div class="nodeContent">
-			<span>
-				Title: {node.title}<br>
-				Position Number: {node.positionNumber}
-			</span>
-			<span>
-				{#if node.employee}
+	<!-- This key block forces the whole component to recreate itself. 
+		For now it's the onl;y way I can find to refresh the compnent in all cases -->
+	{#key root}
+		<TreeNode node={root} childProp="subordinates" let:node>
+			<div class="nodeContent">
+				<span>
 					<span>
-						Employee Number: {node.employee.employeeNumber}<br>
-						Name: {node.employee.firstName} {node.employee.lastName}
+						Title: {node.title}<br>
+						Position Number: {node.positionNumber}
 					</span>
-					<button on:click={() => ShowEditEmployeeModal(node.employee)}> Edit </button>
-					<button on:click={() => UnassignEmployee(node)}> Unassign </button>
-				{:else}
-					Vacant
-					<button>Assign Employee</button>
-				{/if}
-			</span>
-		</div>
-	</TreeNode>
+					<button on:click={() => ShowNewPositionModal(node)}>New Subordinate Position</button>
+				</span>
+				<span>
+					{#if node.employee}
+						<span>
+							Employee Number: {node.employee.employeeNumber}<br>
+							Name: {node.employee.firstName} {node.employee.lastName}
+						</span>
+						<button on:click={() => ShowEditEmployeeModal(node.employee)}> Edit </button>
+						<button on:click={() => UnassignEmployee(node)}> Unassign </button>
+					{:else}
+						Vacant
+						<button on:click={() => ShowEmployeeList(node)}>Assign Employee</button>
+					{/if}
+				</span>
+			</div>
+		</TreeNode>
+	{/key}
 {:else}
 	<p>Loading...</p>
 {/if}
 
-{#if showModal}
+{#if showPositionModal}
+	<div class="modal-background">
+		<div class="modal-input">
+			<p>{newPositionTitle}</p>
+			<div class="modal-input-inner">
+				<label>
+					Position No
+					<input type="number" bind:value={newPosition.positionNumber}/>
+				</label>
+				<label>
+					Position Title
+					<input type="text" bind:value={newPosition.title}/>
+				</label>
+				<span>
+					<button on:click={() => CreateNewPosition(newPosition, selectedSuper.positionNumber)}>Save</button>
+					<button on:click={CloseNewPosition}>Cancel</button>
+				</span>
+
+			</div>
+		</div>
+	</div>
+{/if}
+
+{#if showEmployeeModal}
 	<div class="modal-background">
 		<div class="modal-input">
 			<p>Edit Employee No. {selectedEmployee.employeeNumber}</p>
@@ -110,13 +227,28 @@
 					<input type="text" bind:value={selectedEmployee.lastName}/>
 				</label>
 				<span>
-					<button on:click={() => UpdateEmployee(selectedEmployee)}>Submit</button>
-					<button on:click={CloseModal}>Cancel</button>
+					<button on:click={() => UpdateEmployee(selectedEmployee)}>Save</button>
+					<button on:click={CloseEditEmployee}>Cancel</button>
 				</span>
 
 			</div>
 		</div>
 	</div>
+{/if}
+
+{#if showEmployeeList}
+ <EmployeeList 
+ 	title={listTitle} 
+	items={employeeList} 
+	on:close={CloseEmployeeList}
+	let:item = {employee}>
+	<span>
+		Employee No. {employee.employeeNumber}<br>{employee.firstName}  {employee.lastName}
+	</span>
+	<button on:click={() => AssignEmployee(selectedPosition, employee.employeeNumber)}>
+		Select
+	</button>
+ </EmployeeList>
 {/if}
 
 <style>
